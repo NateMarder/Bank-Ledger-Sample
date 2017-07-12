a12f5e608019acba4821fe64ac8048866cb86246
Nate Marder <nmarder@navexglobal.com>
2017-07-11 23:13:04 -0700

    remove dead code

--- ConsoleBanking/ConsoleProgram.cs
+++ ConsoleBanking/ConsoleProgram.cs
@@ -9,14 +9,8 @@ namespace ConsoleBanking
     public class ConsoleProgram
     {
         private static BankUserOperationsHelper _bankUserOperationsHelper;
-
-        private static TransactionHandler _transactionHandler;
-
         public static BankUserOperationsHelper BankUserOperationsHelper
             => _bankUserOperationsHelper ?? ( _bankUserOperationsHelper = new BankUserOperationsHelper() );
-
-        public static TransactionHandler TransactionHandler
-            => _transactionHandler ?? ( _transactionHandler = new TransactionHandler() );
 
         private static ConsoleDialogHelper _dialogHelper;
 
